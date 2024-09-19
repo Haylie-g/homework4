cc9480d7749f645bc81fe00d0a913cc429b398e2
Haylie Guzman <hguzma15@stu.southtexascollege.edu>
2024-09-19 11:50:05 -0500

    Completed Transaction List Component

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,12 @@
   import Balance from './components/Balance.vue';
   import IncomeExpenses from './components/IncomeExpenses.vue';
   import AddTransaction from './components/AddTransaction.vue';
+  import TransactionList from './components/TransactionList.vue';
 
   import {ref, computed} from 'vue'
 
   const transactions = ref([
-    {id: 1, text:'Paycheck', amount: 700.00},
-    {id: 2, text:'Water Bill', amount: -72.83},
-    {id: 3, text:'Electric Bill', amount: -153.89},
-    {id: 4, text:'Returned Item', amount: 20.00},
+
 ])
 
 const sum = computed(()=>{
@@ -26,6 +24,22 @@ const moneyOut = computed(()=>{
     return acc+x.amount
   },0)
 })
+
+const handleTransaction = (transactionData) => {
+  transactions.value.push({
+    id: generateID(),
+    text: transactionData.text,
+    amount: transactionData.amount,
+  })
+}
+
+const generateID = () => {
+  return Math.floor(Math.random()*10000000)
+}
+const handleDelete = (id) => {
+  transactions.value = transactions.value.filter((x) => x.id !== id)
+}
+
 </script>
 
 <template>
@@ -33,7 +47,8 @@ const moneyOut = computed(()=>{
   <div class="container">
     <Balance :total="sum"></Balance>
     <IncomeExpenses :income="moneyIn":expense="moneyOut"></IncomeExpenses>
-    <AddTransaction></AddTransaction>
+    <AddTransaction @transactionSubmitted="handleTransaction"></AddTransaction>
+    <TransactionList :transactions="transactions" @transactionDeleted="handleDelete"></TransactionList>
 
   </div>
 </template>
